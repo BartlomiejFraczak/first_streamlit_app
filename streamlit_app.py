6f7d87bd3d86081c547ecb1c3d4cdf77765b0b10
Bartłomiej Frączak <71525402+BartlomiejFraczak@users.noreply.github.com>
2024-01-08 09:48:39 +0100

    Update streamlit_app.py
    diner menu setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
-streamlit.title('Yoo I\'ve created a pyathon app! :D')
+streamlit.title('My parents\' new healthy diner:')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
